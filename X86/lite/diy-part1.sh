db1dc699756cf4496ba0eebde3b3aaf15097a510
mugenjie <85976222+mugenjie@users.noreply.github.com>
2024-09-11 06:01:32 +0800

    Update diy-part1.sh

--- X86/lite/diy-part1.sh
+++ X86/lite/diy-part1.sh
@@ -15,6 +15,8 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 echo 'src-git clone https://github.com/sirpdboy/luci-app-ddns-go.git' >>feeds.conf.default
