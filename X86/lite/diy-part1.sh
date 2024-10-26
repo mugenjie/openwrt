dea414282bdb4c03366f67d0e9aa393add5c31ba
mugenjie <85976222+mugenjie@users.noreply.github.com>
2024-10-26 14:10:32 +0800

    Update diy-part1.sh

--- X86/lite/diy-part1.sh
+++ X86/lite/diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 #echo 'src-git clone https://github.com/sirpdboy/luci-app-ddns-go.git' >>feeds.conf.default
-echo 'src-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash' >>feeds.conf.default
+#echo 'src-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash' >>feeds.conf.default
