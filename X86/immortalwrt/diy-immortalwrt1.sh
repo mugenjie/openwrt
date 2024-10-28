89f385d60d06e533d6746ff6a02b865c8c8cd751
mugenjie <85976222+mugenjie@users.noreply.github.com>
2024-10-28 13:38:32 +0800

    Update diy-immortalwrt1.sh

--- X86/immortalwrt/diy-immortalwrt1.sh
+++ X86/immortalwrt/diy-immortalwrt1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >>feeds.conf.default
