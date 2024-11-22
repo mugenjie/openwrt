9f2d52f33212dd2563bf08d397df7430268efd8e
mugenjie <85976222+mugenjie@users.noreply.github.com>
2024-11-22 20:07:00 +0800

    Update diy-LEDE1.sh

--- X86/LEDE/diy-LEDE1.sh
+++ X86/LEDE/diy-LEDE1.sh
@@ -20,4 +20,5 @@
 #echo 'src-git clone https://github.com/sirpdboy/luci-app-ddns-go.git' >>feeds.conf.default
 #echo 'src-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
