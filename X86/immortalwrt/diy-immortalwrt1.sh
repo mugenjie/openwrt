375093d1450b2e64a49cf9f23d743c28264fb213
mugenjie <85976222+mugenjie@users.noreply.github.com>
2025-02-23 09:43:54 +0800

    Update diy-immortalwrt1.sh

--- X86/immortalwrt/diy-immortalwrt1.sh
+++ X86/immortalwrt/diy-immortalwrt1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >>feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >>feeds.conf.default
